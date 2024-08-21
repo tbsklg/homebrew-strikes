529dc854660c1ae363ba311a330e14043287ef1a
Tobias Klug <tobias.klug@codecentric.de>
2024-08-21 15:00:57 +0200

    Initial formula

--- strikes.rb
+++ strikes.rb
@@ -2,7 +2,7 @@ class Strikes < Formula
   desc "A simple CLI tool to track and assign strikes to individuals"
   homepage "https://github.com/tbsklg/strikes"
   url "https://github.com/tbsklg/strikes/releases/download/0.0.2-alpha/strikes-0.0.2-alpha-x86_64-apple-darwin.tar.gz"
-  sha256 "2a015cc2caa02ff223af4b28f31a3fb49f16dc5ce04da3085eee37955661b3be"
+  sha256 "ab565ff0c967c84ff3a8e9dd80208bfd172458afc543ac97cacfe7fbed57d3ea"
   license "MIT license"
 
   def install
