382988f55ddb20bc3e24026fa65d5b5bc722d657
Tobias Klug <tobias.klug@codecentric.de>
2024-08-21 20:59:10 +0200

    Update brew formula

--- strikes.rb
+++ strikes.rb
@@ -1,9 +1,9 @@
 class Strikes < Formula
-  desc "A simple CLI tool to track and assign strikes to individuals"
+  desc "Simple CLI tool to track and assign strikes to things"
   homepage "https://github.com/tbsklg/strikes"
-  url "https://github.com/tbsklg/strikes/releases/download/0.0.2-alpha/strikes-0.0.2-alpha-x86_64-apple-darwin.tar.gz"
+  url "https://github.com/tbsklg/strikes/releases/download/0.0.2/strikes-0.0.2-x86_64-apple-darwin.tar.gz"
   sha256 "ab565ff0c967c84ff3a8e9dd80208bfd172458afc543ac97cacfe7fbed57d3ea"
-  license "MIT license"
+  license "MIT"
 
   def install
     bin.install "strikes"
