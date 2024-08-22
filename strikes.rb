45fe5b1e5dbdc970749b5e0cad15d63d11a2138e
Tobias Klug <tbs.klg@gmail.com>
2024-08-22 09:22:55 +0200

    Update sha256

--- strikes.rb
+++ strikes.rb
@@ -2,7 +2,7 @@ class Strikes < Formula
   desc "Simple CLI tool to track and assign strikes"
   homepage "https://github.com/tbsklg/strikes"
   url "https://github.com/tbsklg/strikes/releases/download/0.1.0/strikes-0.1.0-x86_64-apple-darwin.tar.gz"
-  sha256 "876f57417933f4c4075f2aece2eb3b7542475db0a03c2dfc395633a2658cc576"
+  sha256 "899bf1e439fc9d74705700f376cac6dad6b6c91d2faaf9f0ba890f26da9fd31e"
   license "MIT"
 
   def install
